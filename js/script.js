3cdcffc32b44fb2eb966bd4c32f1ce3e3ed92a4a
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2023-04-04 15:36:24 -0400

    learning to use constructors

--- js/script.js
+++ js/script.js
@@ -1,32 +1,38 @@
-export class Board{
+export class Board {
 
-    main(){
+    main() {
 
-        const grid =[
-            ['','',''],
-            ['','',''],
-            ['','','']
+        const grid = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
         ]
-        
+
         const board = document.querySelector('.board');
-        
+
         let player = 'X';
-        
+
         let b_row = 3;
         let b_column = 3;
-        
-        for(let i = 0; i < b_row; i++){
+
+        for (let i = 0; i < b_row; i++) {
             const row = document.createElement('div');
             row.classList.add('row');
-            for(let j = 0; j < b_column; j++){
+            for (let j = 0; j < b_column; j++) {
                 const cell = document.createElement('div');
                 cell.classList.add('cell');
-                cell.setAttribute('data-row' , i);
+                cell.setAttribute('data-row', i);
                 cell.setAttribute('data-cell', j);
+                cell.addEventListener('click', this.handleClick);
                 row.appendChild(cell);
             }
             board.appendChild(row);
         }
+    }
+
+    handleClick(){
+        const row = this.main.getAttribute('data-row');
+        const col = this.main.getAttribute('data-cell');
     }
 
 }
